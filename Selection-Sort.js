db1635d00dff7a531632aeb8aa09cd2bfb2ed4e8
Kushang Tanawala <kushangtanawala24@gmail.com>
2024-07-19 12:00:40 +0530

    Selection-Sort

---
+++ Selection-Sort.js
@@ -0,0 +1,24 @@
+function selectionSort(arr) {
+    let n = arr.length;
+
+    for (let i = 0; i < n - 1; i++) {
+        let minIndex = i;
+
+        for (let j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        if (minIndex != i) {
+            let temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+    return arr;
+}
+
+let array = [64, 25, 12, 22, 11];
+console.log("Original array: ", array);
+let sortedArray = selectionSort(array);
+console.log("Sorted array: ", sortedArray);
